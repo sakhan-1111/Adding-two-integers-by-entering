5fd6ac517ca515fc92708d9668c2d0aafce33862
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 14:36:25 -0600

    Create Adding two integers by entering.c

---
+++ Adding two integers by entering.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int x,y,sum;
+    printf("Enter the first number = ");
+    scanf("%d", &x);
+    printf("Enter the second number = ");
+    scanf("%d", &y);
+    sum=x+y;
+    printf("The result is = %d",sum);
+    return 0;
+}
